f340350bf6f157848d9dec738c84da5d1d4cb863
soulsam480 <soulsam480@hotmail.com>
2022-02-02 23:19:33 +0530

    feat: pull institute data after creating it

--- src/server/routers/institute.ts
+++ src/server/routers/institute.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from '@trpc/server';
 import { createRouter } from 'server/createRouter';
 import { z } from 'zod';
 
@@ -13,4 +14,17 @@ const cerateDepartmentSchema = z.object({
   instituteId: z.number(),
 });
 
-export const instituteRouter = createRouter();
+export const instituteRouter = createRouter().query('get_institute', {
+  input: z.number(),
+  resolve: async ({ ctx, input }) => {
+    const instituteData = await ctx.prisma.institute.findFirst({ where: { id: input } });
+
+    if (!instituteData)
+      throw new TRPCError({
+        code: 'NOT_FOUND',
+        message: 'Institute not found !',
+      });
+
+    return instituteData;
+  },
+});
