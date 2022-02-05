9a9959478dcbbf79b9e2acf99ed6e4ae5c5df3f3
Manish Kumar Sahu <msahu300@gmail.com>
2022-02-05 19:14:11 +0530

    refactor: student router

--- src/server/routers/accounts.ts
+++ src/server/routers/accounts.ts
@@ -1,6 +1,6 @@
 import { createInstituteSchema } from 'pages/admin/institute/manage/[[...id]]';
+import { createStudentSchema } from 'pages/admin/student/manage/[[...id]]';
 import { createRouter } from 'server/createRouter';
-import { z } from 'zod';
 
 export const accountRouter = createRouter()
   .mutation('create_institute', {
@@ -14,21 +14,7 @@ export const accountRouter = createRouter()
     },
   })
   .mutation('create_student', {
-    input: z.object({
-      code: z.string(),
-      batchId: z.number(),
-      instituteId: z.number(),
-      accountId: z.number(),
-      name: z.string(),
-      dob: z.string(),
-      gender: z.string(),
-      category: z.string(),
-      mobileNumber: z.string(),
-      primaryEmail: z.string(),
-      secondaryEmail: z.string().optional(),
-      permanentAddress: z.string(),
-      currentAddress: z.string(),
-    }),
+    input: createStudentSchema,
     async resolve({ ctx, input }) {
       const student = await ctx.prisma.student.create({
         data: input,
